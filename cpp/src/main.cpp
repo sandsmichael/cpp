90f32bc5ba7d456207377a23584090481cbfe50f
Michael Sands <michaelesands@gmail.com>
2022-04-11 03:26:10 -0400

    debug memcpy error loading texture file

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,6 +1,5 @@
 #include "stdio.h"
 #include "iostream"
-#include "json/json.h"
 
 #include <SFML/Graphics.hpp>
 
@@ -9,9 +8,37 @@ int main()
 
     std::cout << "[Init]" << std::endl;
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    // blue circle
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML Window Title");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Blue);
+
+
+
+    // Create texture from PNG file
+    sf::Texture texture;
+    if (!texture.loadFromFile("assets/sprite.png"))
+    {
+        std::cerr << "Error while loading texture" << std::endl;
+        return -1;
+    }
+    std::cout << "[File Read into Memory]" << std::endl;
+
+    //if (!texture.create(200, 200))
+    //{
+    //    // error...
+    //}
+    // Enable the smooth filter. The texture appears smoother so that pixels are less noticeable.
+    texture.setSmooth(true);
+
+    // Create the sprite and apply the texture
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sf::FloatRect spriteSize = sprite.getGlobalBounds();
+    sprite.setOrigin(spriteSize.width / 2., spriteSize.height / 2.);
+    sprite.setColor(sf::Color(0, 255, 0, 128)); // green
+
+
 
     while (window.isOpen())
     {
@@ -22,10 +49,19 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        // Clear the window and apply grey background
+        window.clear(sf::Color(127, 127, 127));
+
+        // Rotate and draw the sprite
+        sprite.setPosition(50, 50);
+        window.draw(sprite);
+
         window.display();
     }
+    std::cout << "Window has been closed" << std::endl;
+
+
+
 
     return 0;
 }
