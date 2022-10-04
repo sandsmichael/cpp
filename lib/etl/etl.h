825d918394ecc9d2fa5780d471ae1a97f94ec484
Michael Sands <michaelesands@gmail.com>
2022-10-04 17:20:02 -0400

    start etl class

---
+++ lib/etl/etl.h
@@ -0,0 +1,20 @@
+// ETL of .csv file using cpp
+
+#ifndef ETL_h
+#define ETL_h
+
+#include <iostream>
+#include <fstream>
+#include <C:\dev\_external\eigen-git-mirror\Eigen\Dense>
+
+
+class ETL
+{
+
+
+};
+
+
+
+
+#endif
