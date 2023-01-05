fb8f5d3ee41562296b275774b608c371398ace3b
msands <michaelesands@gmail.com>
2023-01-05 01:42:35 -0500

    arrays

---
+++ lib/learn/arr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int orange = 0;
+
+std::vector<std::vector<std::string>> home = {{"A", "B"}, {"Y", "Z"}};
+
+
+int main(){
+    
+    std::cout << "ABC" << std::endl;
+
+
+
+}
